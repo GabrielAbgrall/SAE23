d5eb79d41a218b4e7648c999e970a86a5fbaa577
GabrielAbgrall <gabriel.abgrall@gmail.com>
2024-06-04 09:25:08 +0200

    data management

--- utils.php
+++ utils.php
@@ -8,4 +8,18 @@ function get_template($name) {
     return open_file("src/html/$name.html");
 }
 
+function load_data($path) {
+    return json_decode(fread(
+        fopen("data/$path.json", 'r'),
+        filesize("data/$path.json")
+    ), true);
+}
+
+function save_data($path, $data) {
+    fwrite(
+        fopen("data/$path.json", 'w'),
+        json_encode($data)
+    );
+}
+
 ?>
