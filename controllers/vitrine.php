8c65634c8c6e641fd01e65c8a9b2424e5d94bec8
DomitilleHERITEAU <domitille.heriteau@gmail.com>
2024-06-21 10:13:52 +0200

    ...

--- controllers/vitrine.php
+++ controllers/vitrine.php
@@ -5,7 +5,7 @@ function accueil() {
         require 'views/accueil.php';
     }
 }
-function a-propos() {
+function a_propos() {
     if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         require 'views/a_propos.php';
     }
