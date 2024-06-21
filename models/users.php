1cd76c4d1cd3ff0378c9602d3f553b23d352ec84
lolypop2005 <opale.le-berre@hotmail.fr>
2024-06-21 11:23:31 +0200

    users final

--- models/users.php
+++ models/users.php
@@ -1,27 +1,68 @@
 <?php 
 
-include "utils.php";
-
+require "utils.php";
 
 function get_users(){
     return load_data("users");
 }
 
+function get_user($mail){
+    $users = get_users();
+
+    foreach($users as $user) {
+        if($user['mail'] == $mail) return $user;
+    }
+}
 
 function create_user($name, $firstname, $phone, $mail, $password, $group){
+    if(get_user($mail) != null) return false;
+    
     $user = array(
         "name" => $name,
         "firstname" => $firstname,
         "phone" => $phone,
         "mail" => $mail,
-        "password" => $password,
+        "password" => password_hash($password, PASSWORD_DEFAULT),
         "group" => $group,
     );
 
     $users = get_users();
     array_push($users, $user);
     save_data("users", $users);
+    return true;
 }
 
+function remove_user($mail, $password){
+    if(!check_password($mail, $password))return false;
+    $users = array();
+    foreach(get_users() as $u) {
+        if($u["mail"] != $mail) {
+            array_push($users, $u);
+        }
+    }
+    save_data("users", $users);
+    return true;
+}
+
+function connect($mail, $password){
+    if(!check_password($mail, $password))return false;
+    $user = get_user($mail);
+    session_start();
+    $_SESSION["mail"] = $mail;
+    $_SESSION["name"] = $user["name"];
+    $_SESSION["firstname"] = $user["firstname"];
+    $_SESSION["group"] = $user["group"];
+    return true;
+}
+
+function disconnect(){
+    session_destroy();
+    unset($_SESSION);
+}
+
+function check_password($mail, $password){
+    $user = get_user($mail);
+    return password_verify($password, $user["password"]);
+}
 
 ?>
