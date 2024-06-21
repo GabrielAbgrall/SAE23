b78aca3df95363a1b937d7a53ed9044ae589e35d
DomitilleHERITEAU <domitille.heriteau@gmail.com>
2024-06-21 11:24:43 +0200

    ...

--- views/a_propos.php
+++ views/a_propos.php
@@ -7,18 +7,70 @@
         <h1>Au sujet de l'entreprise</h1>
     </div>
 </div>
-
-<div class="container">
-<h2>Notre entreprise :</h2>
-  <p>Image at the top (card-img-top):</p>
+<br>
+<br>
+<div class="row">
+<div class="col p-3">
   <div class="card" style="width:400px">
     <img class="card-img-top" src="./src/img/batiement.png" alt="Card image" style="width:100%">
     <div class="card-body">
       <h4 class="card-title">37</h4>
-      <p class="card-text">Une hisoire particulière :</p>
+      <p class="card-text">Le siège de Nantes</p>
       <a href="#" class="btn btn-primary">See Profile</a>
     </div>
   </div>
+</div>
+<div class="col p-3">
+  <div class="card" style="width:400px">
+    <img class="card-img-top" src="./src/img/angers.jpeg" alt="Card image" style="width:100%">
+    <div class="card-body">
+      <h4 class="card-title">37</h4>
+      <p class="card-text">Le site d'Angers</p>
+      <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div>
+</div>
+<div class="col p-3">
+  <div class="card" style="width:400px">
+    <img class="card-img-top" src="./src/img/angers.jpeg" alt="Card image" style="width:100%">
+    <div class="card-body">
+      <h4 class="card-title">37</h4>
+      <p class="card-text">Un nouveau site à Saint-Malo</p>
+      <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div>
+</div>
+</div>
+
+<div id="demo" class="carousel slide" data-bs-ride="carousel">
+
+  <!-- Indicators/dots -->
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+  </div>
+  
+  <!-- The slideshow/carousel -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="la.jpg" alt="Los Angeles" class="d-block" style="width:100%">
+    </div>
+    <div class="carousel-item">
+      <img src="chicago.jpg" alt="Chicago" class="d-block" style="width:100%">
+    </div>
+    <div class="carousel-item">
+      <img src="ny.jpg" alt="New York" class="d-block" style="width:100%">
+    </div>
+  </div>
+  
+  <!-- Left and right controls/icons -->
+  <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </button>
 </div>
 
 
