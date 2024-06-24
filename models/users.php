2b6959216e87ff29bf6b6efaa51c2fdf92bf8b92
GabrielAbgrall <gabriel.abgrall@gmail.com>
2024-06-24 19:11:06 +0200

    get_users_with_permission_between

--- models/users.php
+++ models/users.php
@@ -6,7 +6,7 @@ function get_users(){
     return load_data("users");
 }
 
-function get_users_min_permission($level){
+function get_users_with_permission_min($level){
     $users = array();
     foreach(get_users() as $u) {
         if(has_permission($u, $level)) array_push($users, $u);
@@ -14,12 +14,24 @@ function get_users_min_permission($level){
     return $users;
 }
 
-function get_users_with_permission($level){
+function get_users_with_permission_equals($level){
     $users = array();
     require_once 'models/groups.php';
     foreach(get_users() as $u) {
         $g = get_group($u['group']);
         if($g['level'] == $level) array_push($users, $u);
+    }
+    return $users;
+}
+
+function get_users_with_permission_between($min, $max){
+    $users = array();
+    require_once 'models/groups.php';
+    foreach(get_users() as $u) {
+        $g = get_group($u['group']);
+        if($g['level'] >= $min && $g['level'] < $min) {
+            array_push($users, $u);
+        }
     }
     return $users;
 }
