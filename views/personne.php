b6b7ef33e8ca2332357a143fa77fb68abe23be78
DomitilleHERITEAU <domitille.heriteau@gmail.com>
2024-06-24 13:01:35 +0200

    ...

--- views/personne.php
+++ views/personne.php
@@ -15,7 +15,51 @@
 <div>
     <p> fonction php qui affiche les directeurs et le site dont ils sont directeurs</p>
 </div>
-
+<div class="container d-flex flex-row mt-4 justify-content-between">
+<div class="mx-5">
+        <img src="/src/img/pers.png" style="width:150px; height:auto" class="rounded-circle">
+    </div>
+    <div class="col-md-6">
+        <h2>Nom personne</h2>
+        <p>@mail, num, poste</p>
+    </div>
+</div>
+<div class="container">
+    <hr class="featurette-divider">
+</div>
+<div class="container d-flex flex-row mt-4 justify-content-between">
+<div class="mx-5">
+        <img src="/src/img/perso1.jpeg" style="width:150px; height:auto" class="rounded-circle">
+    </div>
+    <div class="col-md-6">
+        <h2>Nom personne</h2>
+        <p>@mail, num, poste</p>
+    </div>
+</div>
+<div class="container">
+    <hr class="featurette-divider">
+</div>
+<div class="container d-flex flex-row mt-4 justify-content-between">
+<div class="mx-5">
+        <img src="/src/img/perso2.jpeg" style="width:150px; height:auto" class="rounded-circle">
+    </div>
+    <div class="col-md-6">
+        <h2>Nom personne</h2>
+        <p>@mail, num, poste</p>
+    </div>
+</div>
+<div class="container">
+    <hr class="featurette-divider">
+</div>
+<div class="container d-flex flex-row mt-4 justify-content-between">
+<div class="mx-5">
+        <img src="/src/img/perso3.jpeg" style="width:150px; height:auto" class="rounded-circle">
+    </div>
+    <div class="col-md-6">
+        <h2>Nom personne</h2>
+        <p>@mail, num, poste</p>
+    </div>
+</div>
 <?php $content = ob_get_clean(); ?>
 
 <?php require 'templates/vitrine.php'; ?>
