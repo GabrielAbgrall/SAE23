1e09ab5361178b7df258afc683fde552c28fbfc3
CarolineBeau <beaumont.caroline.49@gmail.com>
2024-06-25 23:34:13 +0200

    <3

--- views/auth/me.php
+++ views/auth/me.php
@@ -1,27 +1,56 @@
 <?php $title = "Mon compte"; ?>
 
 <?php ob_start(); ?>
+<div class="container">
+    <hr class="featurette-divider">
+</div>
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+                    <h1 class="card-title text-center mb-4">Mon compte</h1>
+                    <form action="/auth/me" method="POST">
+                        <div class="form-group">
+                            <input type="text" class="form-control form-control-lg mb-3" name="name" id="name" placeholder="Nom" required value="<?=$_SESSION['name']?>">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control form-control-lg mb-3" name="firstname" id="firstname" placeholder="Prénom" required value="<?=$_SESSION['firstname']?>">
+                        </div>
+                        <div class="form-group">
+                            <input type="email" class="form-control form-control-lg mb-3" name="mail" id="mail" placeholder="Email" required value="<?=$_SESSION['mail']?>">
+                        </div>
+                        <div class="form-group">
+                            <input type="tel" class="form-control form-control-lg mb-3" name="phone" id="phone" required value="<?=$_SESSION['phone']?>">
+                        </div>
+                        <div class="form-group">
+                            <p class="form-control-lg">Groupe : <?=$_SESSION['group']?></p>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control form-control-lg mb-3" name="new_password" id="new_password" placeholder="Nouveau mot de passe">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control form-control-lg mb-3" name="confirm_new_password" id="confirm_new_password" placeholder="Confirmer le nouveau mot de passe">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control form-control-lg mb-3" name="password" id="password" placeholder="Mot de passe" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-lg btn-block mb-3">Appliquer les modifications</button>
+                        <div class="text-center">
+                            <a href="/" class="btn btn-link btn-lg">Retour à l'accueil</a>
+                            <a href="/auth/unregister" class="btn btn-link btn-lg">Se désinscrire</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
-<form class="container-fluid" action="/auth/me" method="POST">
-    <h1>Mon compte</h1>
+<div class="container">
+    <hr class="featurette-divider">
+</div>
 
-    <input type="text" name="name" id="name" placeholder="Nom" required value="<?=$_SESSION['name']?>">
-    <input type="text" name="firstname" id="firstname" placeholder="Prénom" required value="<?=$_SESSION['firstname']?>">
-    <input type="email" name="mail" id="mail" placeholder="Email" required value="<?=$_SESSION['mail']?>">
-    <input type="tel" name="phone" id="phone" required value="<?=$_SESSION['phone']?>">
-
-    <p>Groupe : <?=$_SESSION['group']?></p>
-    
-    <input type="password" name="new_password" id="new_password" placeholder="Nouveau mot de passe">
-    <input type="password" name="confirm_new_password" id="confirm_new_password" placeholder="Confirmer le nouveau mot de passe">
-
-    <input type="password" name="password" id="password" placeholder="Mot de passe" required>
-
-    <input type="submit" value="Appliquer les modifications'">
-
-    <a href="/">Retour à l'acceuil</a>
-    <a href="/auth/unregister">Se désinscrire</a>
-</form>
 
 <?php $main = ob_get_clean(); ?>
 
