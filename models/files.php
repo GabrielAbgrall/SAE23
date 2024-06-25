0c44da77f1bfdde82f40df5f7464c6122cf3c30a
GabrielAbgrall <gabriel.abgrall@gmail.com>
2024-06-25 23:27:44 +0200

    .

--- models/files.php
+++ models/files.php
@@ -1,9 +1,25 @@
 <?php
 
+require_once 'utils.php';
+
+function get_all_files() {
+    return load_data('drive/files');
+}
+
+function get_files($user) {
+    $files = array();
+    foreach(get_all_files() as $f) {
+        if(has_access($f, $u)) 
+    }
+}
+
 function get_file($path) {
 }
 
 function save_file($path, $file) {
+    if(null !== get_file($path)) return false;
+
+
 }
 
 function delete_file($path) {
@@ -15,7 +31,7 @@ function create_folder($path) {
 function delete_folder($path) {
 }
 
-function has_access($path) {
+function has_access($path, $user) {
 }
 
 ?>
