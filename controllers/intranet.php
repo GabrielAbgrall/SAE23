609ae4883566e8f105398a600dd8b4e4a31ec46a
Yazuu <baptiste.marion15@gmail.com>
2024-06-26 09:02:21 +0200

    fix bug intra_inter

--- controllers/intranet.php
+++ controllers/intranet.php
@@ -21,7 +21,7 @@ function directoryi() {
     if (!has_permission(10)) abort(403);
 
     if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-        require 'views/directory_internal.php';
+        require 'views/intranet/directory_internal.php';
     }
 }
 function directoryc() {
